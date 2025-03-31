3abe8bf1c5739e91422532fca08c0ee7236e19f0
Marco Antonio Russo <marcoantoniorusso94@gmail.com>
2025-03-31 23:43:14 +0200

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MatInputModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
